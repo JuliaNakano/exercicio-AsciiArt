b06ed7895b416bb3791ef3e0126ad5a119f56599
JuliaNakano <jumartinsnakano@gmail.com>
2021-09-07 20:52:15 -0300

    Solução do exercício AsciiArt

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace exercicio_AsciiArt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine( " ||  ||");
+            Console.WriteLine(@" \\()//");
+            Console.WriteLine(@"//(__)\\");
+            Console.WriteLine("||     ||");
+        }
+    }
+}
